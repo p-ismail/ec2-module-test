aeee7646fc4db4539b2c9000df65843995092862
ismail-7337 <ismailp1140@gmail.com>
2026-09-17 10:55:35 +0530

    terraform

--- ec2.tf
+++ ec2.tf
@@ -1,4 +1,11 @@
 module "ec2" {
     source = "../terraform-aws-instance"
+    project = "roboshop"
+    environment = "dev"
     ami_id = "ami-0220d79f3f480ecf5"
+    sg_ids = ["sg-0fc300ed6d5217c2f"]
+    instance_type = "t3.micro"
+    tags = {
+        Name = "roboshop-dev-catalogue"
+    }
 }
